e2f0e6945042d063f395788b02283146b45b6cbb
VcatDeveloper <110053118+VcatDeveloper@users.noreply.github.com>
2022-10-05 08:39:11 -0300

    Create metodos.class.php
    métodos para pegar data do sistema, inserir valores e verificar se o campo está vazio....

---
+++ metodos.class.php
@@ -0,0 +1,54 @@
+<?php
+ //criação da classe contendo métodos dos sistema
+class metodos{
+    //metodo para tratar carcateres do sistema
+    public function tratarCaracter($valor,$tipo){
+        switch($tipo){
+            case 1: $rst = utf8_decode($valor); break;
+            case 2: $rst = htmlentities($valor); break;
+
+        }
+        return $rst;
+
+    }
+
+    //consegue recuperar data e hora
+    public function dataAtual($valor){
+        switch($valor){
+            case 1: return date("Y-m-d"); break;
+            case 2: return date("Y-m-d  H:i:s"); break;
+            case 3: return date("d/m/Y"); break;
+
+        }
+    }
+    
+    //coloca valores na função base64
+
+    public function base64($valor, $n){
+        switch($n){
+            case 1; return base64_encode($valor); break;
+            case 2; return base64_encode($valor); break;
+
+
+        }
+
+
+    }
+    
+    //verfica se o campos está vazio
+
+   public function verificarCampo($dados){
+
+    return (isset($dados)?($dado):("");)
+
+   }
+
+
+}
+
+
+
+
+
+
+?>  
